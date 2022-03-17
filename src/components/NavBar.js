aad936823fa8fa16b45c9703710b7de1c33baa29
Martina Tricarico <tricaricomartina4@gmail.com>
2022-03-16 21:53:34 -0300

    Website

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+function NavBar() {
+  return (
+    <div>
+      <nav className="navbar">
+        <div className="navbar-container"></div>
+      </nav>
+    </div>
+  );
+}
+
+export default NavBar;
